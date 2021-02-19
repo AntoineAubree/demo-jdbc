32a381852c69c14f5bd5a1502940fe1511db306d
DESKTOP-J4CVPG5\Antoine <antoine.aubree@protonmail.com>
2021-02-19 10:53:59 +0100

    add loop

--- src/fr/diginalic/props/TestConfiguration.java
+++ src/fr/diginalic/props/TestConfiguration.java
@@ -3,6 +3,7 @@
  */
 package fr.diginalic.props;
 
+import java.util.Enumeration;
 import java.util.ResourceBundle;
 
 /**
@@ -18,6 +19,13 @@ public class TestConfiguration {
 		ResourceBundle monFichierConf = ResourceBundle.getBundle("database");
 		String prenom = monFichierConf.getString("prenom");
 		System.out.println(prenom);
+		
+		Enumeration<String> keys = monFichierConf.getKeys();
+		while(keys.hasMoreElements()) {
+			String key = keys.nextElement();
+			System.out.println(key);
+			System.out.println(monFichierConf.getString(key));
+		}
 	}
 
 }
